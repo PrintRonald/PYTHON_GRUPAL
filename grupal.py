0cfdebcf2a51906ba892af208dd223378ef7a8cb
marcos675r <marcostoromunoz@gmail.com>
2021-10-21 21:13:20 -0300

    Update grupal.py

--- grupal.py
+++ grupal.py
@@ -1,15 +1,18 @@
 
-print('---INGRESANDO_PROVEEDORES---')
+print('---Bienvenido a la base de datos proveedores/contacto/transacciones---')
 
 info_proveedor = {}
+info_contacto={}
 while True:
     print('---OPCIONES---')
     print("""
     [1] Ingresar proveedor
     [2] Eliminar proveedor
     [3] Mostrar proveedores
-    [4] Guardar Información
-    [5] Salir del programa
+    [4] Ingresar contacto
+    [5] Eliminar contacto
+    [6] Mostrar Contactos
+    [7] Salir del programa
     """)
     n = int(input('Ingrese una opción --> '))
 
@@ -17,7 +20,7 @@ while True:
         IDproveedor = input('Ingrese el identificador del proveedor: ')
         info_proveedor[IDproveedor] = {}
         Nombre_proveedor = input('Ingrese el nombre de la empresa proveedora: ')
-        info_proveedor[IDproveedor]['Nombre'] = Nombre_proveedor
+        info_proveedor[IDproveedor]['Nombre Proveedor'] = Nombre_proveedor
         Rut_proveedor = input('Ingrese el rut del proveedor: ')
         info_proveedor[IDproveedor]['Rut proveedor'] = Rut_proveedor
     elif n == 2:
@@ -26,9 +29,21 @@ while True:
     elif n == 3:
         print(info_proveedor)
     elif n == 4:
-        pass
+       IdContacto = input('Ingrese el ID de contacto: ')
+       info_contacto[IdContacto]={}
+       nombreContacto = input('Ingrese el nombre de contacto: ')
+       info_contacto[IdContacto]['nombreContacto']= nombreContacto
+       telefonoContacto=input('Ingrese el telefono de contacto: ')
+       info_contacto[IdContacto]['Telefono Contacto']= telefonoContacto
+
     elif n == 5:
+        IdContacto = input('Ingrese el identificador del contacto a eliminar: ')
+        del info_contacto[IdContacto]
+    elif n == 6:
+        print(info_contacto)
+    elif n ==7:
         break
+
     else:
         print('Ingrese una opción correcta')
 
